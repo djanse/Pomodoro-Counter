141e883f2add5ed155ebf16a5cfa167cdfa1856c
Danny Janse <d.janse@live.com>
2021-11-13 22:07:49 +0100

    Finish timer

--- Pomodoro Counter/Pomodoro Counter.Designer.cs
+++ Pomodoro Counter/Pomodoro Counter.Designer.cs
@@ -29,8 +29,13 @@ namespace Pomodoro_Counter
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.timerFor = new System.Windows.Forms.TextBox();
             this.timerToggleButton = new System.Windows.Forms.Button();
+            this.pomodoroTimer = new System.Windows.Forms.Timer(this.components);
+            this.pomodoroTimerLabel = new System.Windows.Forms.Label();
+            this.timerStatusBreakLabel = new System.Windows.Forms.Label();
+            this.timerStatusWorkingLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // timerFor
@@ -40,25 +45,66 @@ namespace Pomodoro_Counter
             this.timerFor.Name = "timerFor";
             this.timerFor.PlaceholderText = "What is the timer for?";
             this.timerFor.Size = new System.Drawing.Size(359, 32);
-            this.timerFor.TabIndex = 0;
+            this.timerFor.TabIndex = 2;
             // 
             // timerToggleButton
             // 
-            this.timerToggleButton.Location = new System.Drawing.Point(377, 12);
+            this.timerToggleButton.Location = new System.Drawing.Point(377, 11);
             this.timerToggleButton.Name = "timerToggleButton";
-            this.timerToggleButton.Size = new System.Drawing.Size(75, 32);
+            this.timerToggleButton.Size = new System.Drawing.Size(75, 34);
             this.timerToggleButton.TabIndex = 1;
             this.timerToggleButton.Text = "Start";
             this.timerToggleButton.UseVisualStyleBackColor = true;
             this.timerToggleButton.Click += new System.EventHandler(this.timerToggleButton_Click);
             // 
+            // pomodoroTimer
+            // 
+            this.pomodoroTimer.Interval = 1000;
+            // 
+            // pomodoroTimerLabel
+            // 
+            this.pomodoroTimerLabel.AutoSize = true;
+            this.pomodoroTimerLabel.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.pomodoroTimerLabel.Location = new System.Drawing.Point(12, 47);
+            this.pomodoroTimerLabel.Name = "pomodoroTimerLabel";
+            this.pomodoroTimerLabel.Size = new System.Drawing.Size(283, 128);
+            this.pomodoroTimerLabel.TabIndex = 2;
+            this.pomodoroTimerLabel.Text = "25:00";
+            // 
+            // timerStatusBreakLabel
+            // 
+            this.timerStatusBreakLabel.AutoSize = true;
+            this.timerStatusBreakLabel.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.timerStatusBreakLabel.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.timerStatusBreakLabel.Location = new System.Drawing.Point(303, 114);
+            this.timerStatusBreakLabel.Name = "timerStatusBreakLabel";
+            this.timerStatusBreakLabel.Size = new System.Drawing.Size(149, 45);
+            this.timerStatusBreakLabel.TabIndex = 3;
+            this.timerStatusBreakLabel.Text = "On Break";
+            // 
+            // timerStatusWorkingLabel
+            // 
+            this.timerStatusWorkingLabel.AutoSize = true;
+            this.timerStatusWorkingLabel.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.timerStatusWorkingLabel.Location = new System.Drawing.Point(304, 59);
+            this.timerStatusWorkingLabel.Name = "timerStatusWorkingLabel";
+            this.timerStatusWorkingLabel.Size = new System.Drawing.Size(140, 45);
+            this.timerStatusWorkingLabel.TabIndex = 4;
+            this.timerStatusWorkingLabel.Text = "Working";
+            // 
             // PomodoroCounterWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(464, 281);
+            this.ClientSize = new System.Drawing.Size(464, 183);
+            this.Controls.Add(this.timerStatusWorkingLabel);
+            this.Controls.Add(this.timerStatusBreakLabel);
+            this.Controls.Add(this.pomodoroTimerLabel);
             this.Controls.Add(this.timerToggleButton);
             this.Controls.Add(this.timerFor);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(480, 222);
+            this.MinimumSize = new System.Drawing.Size(480, 222);
             this.Name = "PomodoroCounterWindow";
             this.Text = "Pomodoro Counter";
             this.ResumeLayout(false);
@@ -70,6 +116,10 @@ namespace Pomodoro_Counter
 
         private System.Windows.Forms.TextBox timerFor;
         private System.Windows.Forms.Button timerToggleButton;
+        private System.Windows.Forms.Timer pomodoroTimer;
+        private System.Windows.Forms.Label pomodoroTimerLabel;
+        private System.Windows.Forms.Label timerStatusBreakLabel;
+        private System.Windows.Forms.Label timerStatusWorkingLabel;
     }
 }
 
--- Pomodoro Counter/Pomodoro Counter.cs
+++ Pomodoro Counter/Pomodoro Counter.cs
@@ -1,27 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Media;
 using System.Windows.Forms;
 
 namespace Pomodoro_Counter
 {
     public partial class PomodoroCounterWindow : Form
     {
-        bool timerRunning = false;
+        //Stopwatch pomodoroStopwatch = new Stopwatch();
+        int remainingMinutes = 25;
+        int remainingSeconds = 0;
+        bool onBreak = false;
+
+        SoundPlayer workSoundPlayer = new SoundPlayer("Audio\\work.wav");
+        SoundPlayer breakSoundPlayer = new SoundPlayer("Audio\\break.wav");
 
         public PomodoroCounterWindow()
         {
             InitializeComponent();
+
+            string temp = System.IO.Directory.GetCurrentDirectory();
+            this.workSoundPlayer.Load();
+            this.breakSoundPlayer.Load();
+
+            this.pomodoroTimer.Tick += PomodoroTimer_Tick;
+        }
+
+        private void PomodoroTimer_Tick(object sender, EventArgs e)
+        {
+            //this.pomodoroTimerLabel.Text = this.pomodoroTimer
+            this.remainingSeconds--;
+            if (this.remainingSeconds == -1)
+            {
+                this.remainingSeconds = 59;
+                this.remainingMinutes--;
+            }
+
+            if (this.remainingMinutes == -1)
+            {
+                if (onBreak == true)
+                {
+                    onBreak = false;
+
+                    this.remainingMinutes = 25;
+                    this.remainingSeconds = 0;
+
+                    this.timerStatusWorkingLabel.ForeColor = Color.Black;
+                    this.timerStatusBreakLabel.ForeColor = Color.LightGray;
+
+                    workSoundPlayer.Play();
+                }
+                else
+                {
+                    onBreak = true;
+
+                    this.remainingMinutes = 5;
+                    this.remainingSeconds = 0;
+
+                    this.timerStatusWorkingLabel.ForeColor = Color.LightGray;
+                    this.timerStatusBreakLabel.ForeColor = Color.Black;
+
+                    breakSoundPlayer.Play();
+                }
+            }
+
+            string minutesString = (this.remainingMinutes < 10) ? "0"  : "";
+            string secondsString = (this.remainingSeconds < 10) ? "0"  : "";
+
+            minutesString += this.remainingMinutes;
+            secondsString += this.remainingSeconds;
+
+            this.pomodoroTimerLabel.Text = minutesString + ":" + secondsString;
         }
 
         private void timerToggleButton_Click(object sender, EventArgs e)
         {
-            if (this.timerRunning == true)
+            if (this.pomodoroTimer.Enabled == true)
             {
                 this.stopTimer();
             }
@@ -34,13 +89,15 @@ namespace Pomodoro_Counter
         private void stopTimer()
         {
             this.timerToggleButton.Text = "Start";
-            this.timerRunning = false;
+            pomodoroTimer.Stop();
+            breakSoundPlayer.Play();
         }
 
         private void startTimer()
         {
             this.timerToggleButton.Text = "Stop";
-            this.timerRunning = true;
+            pomodoroTimer.Start();
+            workSoundPlayer.Play();
         }
     }
 }
